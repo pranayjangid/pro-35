4f59960214a5969f6321f891eb34359775a201a0
pranayjangid <74404469+pranayjangid@users.noreply.github.com>
2021-05-03 16:00:11 +0530

    commit

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var balloon, background;
+var position, database;
+
+function preload() {
+  backgroundImage = loadImage("images/1.png");  
+  balloonImage = loadAnimation("images/2.png","images/3.png","images/4.png");
+}
+
+function setup(){
+   database = firebase.database();
+   console.log(database);
+   createCanvas(500,500);
+
+   balloon = createSprite (100,400, 20, 20)
+   balloon.addAnimation("balloon", balloonImage);
+   balloon.scale = 0.4;
+
+   balloonRef = database.ref("balloon/position");
+   balloonRef.on("value",function(data){
+    position = data.val();
+    console.log(position.x);
+    balloon.x = position.x;
+    balloon.y = position.y;
+   });
+}
+
+function draw(){
+
+    background(backgroundImage);
+  
+        if(keyDown(LEFT_ARROW)){
+            updateBalloonPosition(-10,0);            
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            updateBalloonPosition(10,0);  
+        }
+        else if(keyDown(UP_ARROW)){
+            updateBalloonPosition(0,-10);  
+        }
+        else if(keyDown(DOWN_ARROW)){
+            updateBalloonPosition(0,10);  
+        }
+        drawSprites();
+}
+
+function updateBalloonPosition(x,y){
+    database.ref("balloon/position").update({
+        x:position.x+x,
+        y:position.y+y,
+    })
+}
+
+
+    
+
+
